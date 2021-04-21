dff400e167db90059570f7e167c1d261961fa24e
Panchop10 <folivares@arenasoft.cl>
2021-04-21 18:54:36 +0000

    remove unused files from xml/xsd version

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,31 +1,4 @@
-const path = require('path'); //The path module provides utilities for working with file and directory paths
-const fs = require('fs'); //This module allows to work witht the file system: read and write files back
-const xml2js = require('xml2js'); //This module does XML to JSON conversion and also allows us to get from JSON back to XML
-const xmlParse = require('xslt-processor').xmlParse; //This module allows us to work with XML files
-const xsltProcess = require('xslt-processor').xsltProcess; //The same module allows us to utilise XSL Transformations
-const { v4: uuid_v4 } = require('uuid');
-const { exit } = require('process');
-
 var Product = require('../models/product.model')
-
-
-// Function to read in XML file and convert it to JSON
-function xmlFileToJs(filename, cb) {
-  var filepath = path.normalize(path.join(__dirname, filename));
-  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-    if (err) throw (err);
-    xml2js.parseString(xmlStr, {}, cb);
-  });
-}
-
-//Function to convert JSON to XML and save it
-function jsToXmlFile(filename, obj, cb) {
-  var filepath = path.normalize(path.join(__dirname, filename));
-  var builder = new xml2js.Builder();
-  var xml = builder.buildObject(obj);
-  fs.unlinkSync(filepath);
-  fs.writeFile(filepath, xml, cb);
-}
 
 module.exports = {
   name: 'productsController',
